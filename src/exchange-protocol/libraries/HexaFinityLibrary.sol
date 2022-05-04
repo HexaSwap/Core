1c5f045366801e7f5ede7be1b31a9b02450b5d1b
Curest <curest.trebow@gmail.com>
2022-05-05 05:50:57 +0900

    Update INIT_CODE_HASH

--- src/exchange-protocol/libraries/HexaFinityLibrary.sol
+++ src/exchange-protocol/libraries/HexaFinityLibrary.sol
@@ -22,7 +22,7 @@ library HexaFinityLibrary {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'ed1ab5ebb3572baaf9f4fafa39901475c99c51a3a9fd2a5ca42982b6d1899811' // init code hash
             ))));
     }
 
